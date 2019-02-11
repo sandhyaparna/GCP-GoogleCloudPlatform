dddeea39972bdf4c3ffe2cb7fec838b2c359636e
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2019-02-11 11:15:25 -0600

    Update EstimatorAPI.py

--- Tensorflow/EstimatorAPI.py
+++ Tensorflow/EstimatorAPI.py
@@ -1,3 +1,4 @@
+# https://github.com/sandhyaparna/training-data-analyst/blob/master/courses/machine_learning/deepdive/03_tensorflow/b_estimator.ipynb
 # https://googlecoursera.qwiklabs.com/focuses/25433?locale=en
 
 featcols = [
@@ -5,8 +6,16 @@ featcols = [
       tf.feature_column.categorical_column_with_vocabulary_list("type",["house","apt"]), #type variable has 2 unique values-house, apt
       tf.feature_column_numeric_column("nbeds")
 ]
-
 model = tf.estimator.LinearRegressor(featcols,'./model_trained') #'./model_trained' is used to create checkpoints in that folder
+
+# featcols can also be - To extract only numeric cols
+CSV_COLUMNS = ['fare_amount', 'pickuplon','pickuplat','dropofflon','dropofflat','passengers', 'key']
+FEATURES = CSV_COLUMNS[1:len(CSV_COLUMNS) - 1]
+LABEL = CSV_COLUMNS[0]
+def feature_cols():
+  input_columns = [tf.feature_column.numeric_column(k) for k in FEATURES]
+  return input_columns
+model = tf.estimator.LinearRegressor(feature_columns = feature_cols(), model_dir = OUTDIR)
 
 def train_input_fn(df):
   return tf.estimator.inputs.pandas_input_fn(
