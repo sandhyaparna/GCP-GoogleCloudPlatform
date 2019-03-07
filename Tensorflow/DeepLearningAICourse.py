92ca68f1a4e474c27f21887ad91d044554bac93b
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2019-03-07 13:43:23 -0600

    Update DeepLearningAICourse.py

--- Tensorflow/DeepLearningAICourse.py
+++ Tensorflow/DeepLearningAICourse.py
@@ -5,6 +5,14 @@
 # Sequential - Successive layers are defined in Sequence
 model = keras.sequential([keras.layers.Dense(units=1, input_shape=[1])])
 
+# 3 layers
+# first layer corresponds to input - shape to be expected for the data to be in
+# Last layer corresponds to diff Target classes
+# Hidden Layer - 128 neurons - 
+model = tf.keras.models.Sequential([tf.keras.layers.Flatten(input_shape=(28,28)), 
+                                    tf.keras.layers.Dense(128, activation=tf.nn.relu), 
+                                    tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
+
 # Loss functions - Measure how good the current guess is
 # Optimizer - First NN starts with a guess and then optimizer is used to improve upon it. It generates a new and improved guess
 # (sgd - stochastic gradient descent)
@@ -15,6 +23,9 @@ model.compile(optimizer='sgd', loss='mean_squared_error')
 # epochs - Number of training loops 
 model.fit(xs, ys, epochs=500)
 model.predict([y])
+
+
+
 
 
 
